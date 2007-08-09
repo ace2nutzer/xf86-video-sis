93baf379ddf2ffd77fbf46804bec7fcf1202ef62
Julien Cristau <jcristau@debian.org>
2007-08-09 03:36:07 +0200

    Get rid of the XFree86Server macro.
    It's not exported by xserver these days, so don't test it.

--- src/sis_dri.h
+++ src/sis_dri.h
@@ -116,13 +116,10 @@ typedef struct {
   int dummy;
 } SISDRIContextRec, *SISDRIContextPtr;
 
-#ifdef XFree86Server
-
 #include "screenint.h"
 
 Bool SISDRIScreenInit(ScreenPtr pScreen);
 void SISDRICloseScreen(ScreenPtr pScreen);
 Bool SISDRIFinishScreenInit(ScreenPtr pScreen);
 
-#endif
 #endif
