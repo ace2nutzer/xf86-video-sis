224828e56d8701df97fdfa2c2abc4cd6afa6118e
Adam Jackson <ajax@redhat.com>
2012-09-25 08:54:51 -0400

    Remove mibstore.h
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- src/sis_driver.c
+++ src/sis_driver.c
@@ -84,7 +84,6 @@
 #include "fb.h"
 #include "micmap.h"
 #include "mipointer.h"
-#include "mibstore.h"
 #include "edid.h"
 
 #define SIS_NEED_inSISREG
@@ -9523,7 +9522,6 @@ SISScreenInit(SCREEN_INIT_ARGS_DECL)
     }
     pSiS->SiSFastVidCopyDone = TRUE;
 
-    miInitializeBackingStore(pScreen);
     xf86SetBackingStore(pScreen);
     xf86SetSilkenMouse(pScreen);
 
