5966f4a8d2ad0f5bf2c4878aeaf9ef3e8d9b1294
ace2nutzer <ace2nutzer@gmail.com>
2024-08-22 22:32:36 +0200

    autogen.sh: update

--- autogen.sh
+++ autogen.sh
@@ -5,21 +5,23 @@ if [ -f Makefile ] ; then
 	make -j2 distclean
 fi
 
+autoupdate
+
 srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
 cd "$srcdir"
 
-autoreconf -v --install || exit 1
+autoreconf --force -v --install || exit 1
 cd "$ORIGDIR" || exit $?
 
-CFLAGS_CPU="-march=core2 -mcpu=core2 -mtune=core2 -mhard-float -mfpmath=sse"
+FLAGS_CPU="-march=native -mcpu=native -mtune=native"
 
 if test -z "$NOCONFIGURE"; then
     exec "$srcdir"/configure \
-		CFLAGS="$CFLAGS_CPU -O2 -ftree-vectorize -fomit-frame-pointer -fno-strict-aliasing \
+		CFLAGS="$FLAGS_CPU -O2 -ftree-vectorize -fomit-frame-pointer -fno-strict-aliasing \
 		-Werror-implicit-function-declaration -Wno-stringop-overflow -Wno-redundant-decls \
 		-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -DNDEBUG -pipe" \
-		--prefix=/usr "$@"
+		--prefix=/usr/local "$@"
 fi
