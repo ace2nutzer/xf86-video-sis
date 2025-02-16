41c9e5d7e4404122b39b85d904ba6497ba99e7e1
ace2nutzer <ace2nutzer@gmail.com>
2025-02-16 04:12:08 +0100

    autogen.sh: update

--- autogen.sh
+++ autogen.sh
@@ -22,8 +22,9 @@ FLAGS_CPU="-march=native -mcpu=native -mtune=native"
 
 if test -z "$NOCONFIGURE"; then
     exec "$srcdir"/configure \
-		CFLAGS="$FLAGS_CPU -O2 -ftree-vectorize -fomit-frame-pointer -fno-strict-aliasing \
-		-Werror-implicit-function-declaration -Wno-stringop-overflow -Wno-redundant-decls \
+		CFLAGS="$FLAGS_CPU -O2 \
+		-fomit-frame-pointer -fno-strict-aliasing \
+		-Werror-implicit-function-declaration -Wno-redundant-decls \
 		-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable \
 		-Werror=incompatible-pointer-types \
 		-DNDEBUG -pipe" \
